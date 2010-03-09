cb16f0d9bbd34a196c05648c78ab24c016e837bc
Jacek Bzdak <jbzdak@gmail.com>
2010-03-09 17:03:52 +0100

    init

---
+++ src/main/java/cx/ath/jbzdak/aztec/Point.java
@@ -0,0 +1,18 @@
+package cx.ath.jbzdak.aztec;
+
+/**
+ * @author Jacek Bzdak jbzdak@gmail.com
+ *         Date: Mar 9, 2010
+ */
+public class Point {
+   
+   double x, y;
+
+   public Point() {
+   }
+
+   public Point(double x, double y) {
+      this.x = x;
+      this.y = y;
+   }
+}
